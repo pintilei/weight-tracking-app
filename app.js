619189c8c78665fb8d52971369a91c281b4dcdb9
pintilei <pintileigraphics@gmail.com>
2018-07-22 20:24:58 +0300

    Set daily cronjob to send a reminder email if there are no tracker logs for that day, use .env file to store sensitive data such as email password

--- app.js
+++ app.js
@@ -1,28 +1,77 @@
+require('dotenv').load();
 var express = require('express');
+var moment = require('moment');
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 var bodyParser = require('body-parser')
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.urlencoded({extended: true}))
 
 // Connect to mongodb using mongoose
 var mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost:27017/weight-tracking", {
-  useNewUrlParser: true
- });
+    useNewUrlParser: true
+});
+
+//Set CronJob to send daily reminder email if there are no logs for that day
+var CronJob = require('cron').CronJob;
+var nodemailer = require('nodemailer');
+
+new CronJob('00 30 19 * * 0-6', function () { // 19:30, each day
+    console.log('Start cron job...');
+
+    var start = moment().startOf('day');
+    var end = moment().endOf('day');
+
+    Tracker.find({
+        "date": {$gte: start, $lt: end}
+    }).countDocuments((err, count) => {
+        if (!count) {
+
+            console.log(count + ' results found, should send notification email');
+
+            var transporter = nodemailer.createTransport({
+                host: process.env.MAIL_HOST,
+                port: process.env.MAIL_PORT,
+                secure: true,
+                auth: {
+                    user: process.env.MAIL_AUTH_USER,
+                    pass: process.env.MAIL_AUTH_PASS
+                }
+            });
+
+            var mailOptions = {
+                from: process.env.MAIL_AUTH_USER,
+                to: process.env.MAIL_RECEIVER,
+                subject: 'New message from Weight Tracker',
+                text: 'Don\'t forget to fill in your weight.'
+            };
+
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
+        } else {
+            console.log(count + ' results found, do not send notification email');
+        }
+    });
+}, null, true, 'Europe/Bucharest');
 
 // Set database schema
 var trackerSchema = new mongoose.Schema({
-   weight: {
-     type: Number,
-     required: [true, 'Please provide weight']
-   },
-   date: {
-     type: Date,
-     default: Date.now
-   }
- });
+    weight: {
+        type: Number,
+        required: [true, 'Please provide weight']
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
 var Tracker = mongoose.model('Tracker', trackerSchema);
 
 // Set the static directory for assets
@@ -32,26 +81,22 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 // Routes
-app.get('/', function(req, res){
-  Tracker.find({}).sort('-date').exec((err, trackerLogs) => {
-      res.render('pages/index', { trackerLogs: trackerLogs })
-  });
+app.get('/', function (req, res) {
+    Tracker.find({}).sort('-date').exec((err, trackerLogs) => {
+        res.render('pages/index', {trackerLogs: trackerLogs})
+    });
 });
 
-app.get('/track', function(req, res){
-  res.render('pages/track');
+app.get('/track', function (req, res) {
+    res.render('pages/track');
 });
 
-app.post('/track', function(req, res){
-  var trackerData = new Tracker(req.body);
-
-    var error = trackerData.validateSync();
-    console.log(error);
-
-    trackerData.save().then( result => {
+app.post('/track', function (req, res) {
+    var trackerData = new Tracker(req.body);
+    trackerData.save().then(result => {
         res.redirect('/');
     }).catch(err => {
-        res.status(400).send("Unable to save data");
+        res.status(400).send(err);
     });
 });
 
